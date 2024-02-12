5f88bc2e0f09e81b7db489ad04e682d0803a9c11
LightingTom <1452442064@qq.com>
2024-02-11 18:22:40 -0800

    Adjust beat bar to fit in the new map

--- CSCI526_prototype1/Assets/Scripts/TriggerHandler.cs
+++ CSCI526_prototype1/Assets/Scripts/TriggerHandler.cs
@@ -10,12 +10,12 @@ public class TriggerHandler : MonoBehaviour
     {
         if (other.CompareTag("Obstacle") && gameController != null && !Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl))
         {
-            Debug.Log(other.gameObject.name + " : " + gameObject.name);
+            // Debug.Log(other.gameObject.name + " : " + gameObject.name);
             gameController.GameOver();
         }
         if (other.CompareTag("Finish") && gameController != null)
         {
-            Debug.Log(other.gameObject.name + ": Victory");
+            // Debug.Log(other.gameObject.name + ": Victory");
             gameController.victory();
         }
     }
